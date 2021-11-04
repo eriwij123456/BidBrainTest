85ff970c11b24bdf729f925736bb4266bd8492b8
Erik Wijnbladh <wijnbladherik@gmail.com>
2021-11-04 19:31:29 +0100

    Final touches - added a few images for proof of concept

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
 	<div class="bg-gradient-to-l from-gray-600 to-gray-400 fixed w-screen h-screen flex overflow-y-auto flex-col">
-		<div class="min-w-0 mx-auto my-2">
+		<div class="min-w-0 mx-auto mt-6 mb-4">
 			<input v-model="searchQuery" type="text" placeholder="Search..." class="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900">
 		</div>
-		<div class="mx-auto w-3/4 sm:w-2/3 px-4 py-4">
-			<div class="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-6">
-				<div @click="openMovie(bond)" class="flex-1 flex flex-col p-4 sm:p-8 bg-white rounded-md text-center hover:opacity-75 cursor-pointer" v-for="bond in filteredList" :key="bond">
-					<img class="w-16 h-16 sm:w-32 sm:h-32 flex-shrink-0 mx-auto rounded-full mb-2" src="https://bit.ly/3EIwWVE" alt="">
-					<p class="text-xs sm:text-sm font-bold">{{ bond.title }}</p>
+		<div class="mx-auto w-full xl:w-2/3 px-4 py-4">
+			<div class="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-6 lg:h-2/3 sm:w-2/3 mx-auto">
+				<div @click="openMovie(bond)" v-for="bond in filteredList" :key="bond" class="hover:opacity-75 cursor-pointer">
+					<p class="hidden lg:block text-sm font-bold text-center bg-gray-50 rounded-t-md py-2 text-lg">{{ bond.title }}</p>
+					<div class="flex-1 flex flex-col p-4 lg:p-6 bg-gray-50 rounded-md lg:rounded-t-none text-center overflow-hidden h-full lg:h-auto">
+						<img v-if="bond.image" class="hidden lg:block w-full h-full flex-shrink-0 mx-auto transform rotate-6" :src="bond.image">
+						<div v-else class="animate-pulse flex justify-center h-full w-full">
+							<div class="rounded-md bg-indigo-200 p-20 transform rotate-6"></div>
+						</div>
+						<img class="w-16 h-16 lg:hidden flex-shrink-0 mx-auto rounded-full" src="https://bit.ly/3EIwWVE">
+						<p class="lg:hidden text-xs sm:text-sm font-bold my-2 items-end">{{ bond.title }}</p>
+					</div>
 				</div>
 				<Moviecard v-if="dialog" @closeDialog="closeDialog()" :movie="selectedMovie"/>
 			</div>
@@ -45,7 +52,7 @@ export default {
 		filteredList(){
 			if (this.searchQuery){
 				return this.sortable.filter((bond)=>{
-					return this.searchQuery.toLowerCase().split(' ').every(v => bond.title.toLowerCase().includes(v))
+					return this.searchQuery.toLowerCase().split(' ').every(v => bond.title.toLowerCase().includes(v) || bond.director.toLowerCase().includes(v) || bond.actor.toLowerCase().includes(v))
 				})
 			} else {
 				return this.sortable;
--- src/components/Moviecard.vue
+++ src/components/Moviecard.vue
@@ -1,24 +1,24 @@
 <template>
 <div class="fixed z-10 inset-0 overflow-y-scroll" aria-labelledby="modal-title" role="dialog" aria-modal="true">
 	<div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-		<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+		<div @click="closeDialog()" class="fixed inset-0 bg-gray-700 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
 		<span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 		<div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
 			<p class="absolute top-2 right-4 font-bold text-gray-600 cursor-pointer" @click="closeDialog()">X</p>
 			<div>
 				<img class="w-16 h-16 sm:w-32 sm:h-32 flex-shrink-0 mx-auto rounded-full mb-2" src="https://bit.ly/3EIwWVE" alt="">
 				<div class="mt-3 text-center sm:mt-5">
-					<h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+					<h3 class="text-lg leading-6 font-bold text-gray-900" id="modal-title">
 						{{movie.title}}
 					</h3>
 					<div class="mt-2">
-						<p class="text-sm text-gray-500">
+						<p class="text-md text-gray-600 font-medium">
 							Director: {{movie.director}}
 						</p>
-						<p class="text-sm text-gray-500">
+						<p class="text-md text-gray-600 font-medium">
 							Bond: {{movie.actor}}
 						</p>
-						<p class="text-sm text-gray-500">
+						<p class="text-md text-gray-600 font-medium">
 							Year: {{movie.year}}
 						</p>
 					</div>
