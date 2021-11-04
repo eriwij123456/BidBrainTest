cc2a0fcba6deb814bdd497c152b9f48c27d354e3
Erik Wijnbladh <wijnbladherik@gmail.com>
2021-11-04 16:27:19 +0100

    Scaffolding

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+
+
+export default {
+  	name: 'App',
+  	data() {
+		return {	
+		}
+  	},
+
+  	components: {
+		HelloWorld
+  	},
+
+	props: {
+	},
+
+	computed: {
+  	},
+
+	mounted () {
+  	},
+
+	methods: {
+	},
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+	<h1>{{ msg }}</h1>
+
+	<p>
+	Recommended IDE setup:
+	<a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+	+
+	<a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+	</p>
+
+	<p>
+	<a href="https://vitejs.dev/guide/features.html" target="_blank">
+		Vite Documentation
+	</a>
+	|
+	<a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+	</p>
+
+	<button type="button" @click="count++">count is: {{ count }}</button>
+	<p>
+	Edit
+	<code>components/HelloWorld.vue</code> to test hot module replacement.
+	</p>
+</template>
+
+<script>
+
+export default {
+  	data() {
+		return {
+			count = ref(0)	
+		}
+  	},
+
+  	components: {
+		Moviecard
+  	},
+
+	props: {
+		msg: String
+	},
+
+	computed: {
+  	},
+
+	mounted () {
+  	},
+
+	methods: {
+	},
+}
+</script>
