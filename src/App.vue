1f89354596a3e6131a5091f62a107482569099df
Erik Wijnbladh <wijnbladherik@gmail.com>
2021-11-04 16:37:43 +0100

    Grid listing all Bond movies with corresponding title added

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-	<img alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Hello Vue 3 + Vite" />
+	<div class="bg-gradient-to-l from-gray-600 to-gray-400 fixed w-screen h-screen flex justify-between overflow-y-auto">
+		<div class="mx-auto my-auto px-4 py-4">
+			<div class="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-6" v-for="movie in movies" :key="movie">
+				<div  class="flex-1 flex flex-col p-4 sm:p-8 bg-white rounded-md text-center" v-for="bond in movie" :key="bond">
+					<img class="w-16 h-16 sm:w-32 sm:h-32 flex-shrink-0 mx-auto rounded-full mb-2" src="https://bit.ly/3EIwWVE" alt="">
+					<p class="text-xs sm:text-sm font-bold">{{ bond.title }}</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 
-import HelloWorld from './components/HelloWorld.vue'
+import movies from '../data/movies.json'
 
 
 export default {
   	name: 'App',
   	data() {
 		return {	
+			movies: movies,
 		}
   	},
 
   	components: {
-		HelloWorld
+		
   	},
 
 	props: {
